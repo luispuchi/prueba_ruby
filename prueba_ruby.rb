90c14c6a15bda1c53ca12a4cfba49acecc30885b
luis puchi <puchiluis11@gmail.com>
2020-06-08 18:32:48 -0400

    actualizacion

--- prueba_ruby.rb
+++ prueba_ruby.rb
@@ -3,63 +3,54 @@ require 'net/http'
 require 'json'
 require 'openssl'
 
-def request(url_addres, api_key='pGocQGbUuPUQ7mi8HzvKs2PLTCLrUF8Cd0iYschR')
-  url = URI("#{url_addres}&api_key=#{api_key}")
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  request = Net::HTTP::Get.new(url)
-  request["User-Agent"] = 'PostmanRuntime/7.15.0'
-  request["Accept"] = '*/*'
-  request["Cache-Control"] = 'no-cache'
-  request["Postman-Token"] = '95f7d1a0-04cf-40b1-8fba-66535ca6031a,59f71849-e627-46be-aaa0-7fe592a10416'
-  request["Host"] = 'api.nasa.gov'
-  request["cache-control"] = 'no-cache'
-  response = http.request(request)
-  JSON.parse(response.read_body)
-end
 
-
-
-def buid_web_page(hash)
-html = '<html>
-  <head>
-  </head>
-  <body>
-  <ul>'
-
-  hash['photos'].each do |a|
-    html += "<li><img src='#{a['img_src']}'></li>"
+class RequestNasa
+  def initialize (url, api_key)
+    @url = url
+    @api_key = api_key
+    @uri = URI("#{@url}&api_key=#{@api_key}")
   end
 
-html += '
-  </ul>
-  </body>
-  </html>'
-
-
-File.open("index.html", "w+") do |f |
-    f.write(html)
+  def request
+    http = Net::HTTP.new(@uri.host, @uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(@uri)
+    response = http.request(request)
+    hash = JSON.parse(response.read_body)
+    buid_web_page(hash)
   end
-end
 
-def photos_count(hash)
-hash_x = {}
+  def buid_web_page(hash)
+    html = '<html>
+      <head>
+      </head>
+      <body>
+      <ul>'
 
-hash["photos"].each do |i |
-    nombre = i["camera"]["name"]
-    if (hash_x[nombre])
-        hash_x[nombre] += 1
-    else
-        hash_x[nombre] = 1
+      hash['photos'].each do |a|
+        html += "<li><img src='#{a['img_src']}'></li>"
+      end
+
+    html += '
+      </ul>
+      </body>
+      </html>'
+
+
+    File.open("index.html", "w+") do |f|
+      f.write(html)
     end
-end
+    photos_count(hash)
+  end
 
-puts hash_x
+  def photos_count(hash)
+    new_hash = []
+    hash['photos'].each{|h| new_hash << {name: h['camera']['name'], quantity: h['rover']['total_photos']}}
+    puts new_hash
+  end
 end
-
-api_key = 'pGocQGbUuPUQ7mi8HzvKs2PLTCLrUF8Cd0iYschR'
-url_addres = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10"
-hash = request(url_addres, api_key)
-buid_web_page(hash)
-photos_count(hash)
+api_key = 'pGocQGbUuPUQ7mi8HzvKs2PLTCLrUF8Cd0iYschR' 
+url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10'
+obj = RequestNasa.new(url, api_key)
+hash = obj.request
